ebacbadd35c8f50076de255626f3efb3e64a7a2b
Chinmay Roy <39342322+chinmayroy@users.noreply.github.com>
2021-07-18 03:48:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Take two variable value
+a = int(input("Enter first number: "))
+b = int(input("Enter first number: "))
+
+#summation of two variable
+sum = (a+b)
+
+#print the summation value for final result display 
+print("Total Summation Value is:", sum)
